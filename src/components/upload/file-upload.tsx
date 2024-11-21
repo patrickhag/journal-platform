a34926eda09bc26d5fd47cc005054531960684d5
muslimuwitondanishema <muslimuwitondanishema@gmail.com>
2024-11-21 07:57:19 +0200

    feat: :sparkles: Add upload file and delete file
    ghstack-source-id: c1c67a31d44b391b582627cef8e96d52c9f9759b
    Pull Request resolved: https://github.com/patrickhag/journal-platform/pull/8

---
+++ src/components/upload/file-upload.tsx
@@ -0,0 +1,53 @@
+"use client"
+import { Card } from "@/components/ui/card"
+import { Upload } from 'lucide-react'
+import { useSearchParams } from "next/navigation"
+import { useState } from "react"
+import { CldUploadWidget, CloudinaryUploadWidgetInfo } from "next-cloudinary";
+import { Sidebar } from "../Sidebar"
+import { Paginator } from "./Paginator"
+import { FileList } from "./FileList"
+import { ProgressLine } from "./Progress"
+import { Uploadbanner } from "./Uploadbanner"
+
+const fileFormats = ["DOCX", "DOC", "PDF"];
+
+export default function FileUpload() {
+    const [files, setFiles] = useState<CloudinaryUploadWidgetInfo[]>([]);
+    const searchParams = useSearchParams()
+
+    return (
+        <div className="flex h-screen bg-gray-100">
+            <Sidebar />
+
+            <main className="flex-1 p-10">
+                <ProgressLine />
+                <h1 className="mb-6 text-3xl font-bold">Upload files</h1>
+                <Card className="mb-6 p-6">
+                    <CldUploadWidget
+
+                        uploadPreset={process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET}
+                        signatureEndpoint="/api/sign-cloudinary-params"
+                        onSuccess={(result) => {
+                            if (typeof result.info === "object" && "secure_url" in result.info) {
+                                setFiles(prev => [...prev, result.info as CloudinaryUploadWidgetInfo]);
+                            }
+                        }}
+
+                        options={{
+                            folder: "journal_upload",
+                            clientAllowedFormats: fileFormats,
+                            multiple: true
+                        }}
+                    >
+                        {({ open }) => <Uploadbanner fileFormats={fileFormats} open={open}/>}
+                    </CldUploadWidget>
+                    <div className="space-y-4">
+                        <FileList files={files} setFiles={setFiles} />
+                    </div>
+                </Card>
+                <Paginator />
+            </main>
+        </div>
+    )
+}
